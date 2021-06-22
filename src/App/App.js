c8527fca0c48e7e57942b6bee49cc79c1679cc24
Ameur <ameur@craftfoundry.tech>
2021-06-22 11:13:34 +0100

    fix storybook dep

--- src/App/App.js
+++ src/App/App.js
@@ -1,70 +1,9 @@
-import React, { useState } from "react";
-import classNames from "classnames";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { ThemeProvider } from "@material-ui/core";
-import SettingsIcon from "@material-ui/icons/Settings";
-import DriveEtaIcon from "@material-ui/icons/DriveEta";
-import PostAddIcon from "@material-ui/icons/PostAdd";
-import DescriptionIcon from "@material-ui/icons/Description";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import SideMenu from "../components/SideMenu/SideMenu";
-import IconDrawer from "../components/SideMenu/IconDrawer";
-import theme from "./theme";
+import React from "react";
 import styles from "./style";
-import ListProjects from "../components/ListProjects/ListProjects";
-import Header from "../components/Header/Header";
-
 function App() {
   const classes = styles();
-  const [open, setOpen] = useState(true);
-
-  const handleDrawerOpen = () => {
-    setOpen(!open);
-  };
-
-  const menuItems = [
-    { href: "/", icon: <PostAddIcon />, title: "Page 1" },
-    { href: "/page2", icon: <SettingsIcon />, title: "Page 2" },
-    { href: "/page3", icon: <DescriptionIcon />, title: "Page 3" },
-    { href: "/page4", icon: <DriveEtaIcon />, title: "Page 4" },
-  ];
-
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Header />
-      <Drawer
-        variant="permanent"
-        className={classNames(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: classNames({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-        open={open}
-      >
-        <SideMenu menuItems={menuItems} />
-        <div className={!open ? classes.toolbarOpen : classes.toolbarClose}>
-          <IconDrawer handleDrawerOpen={handleDrawerOpen} open={open} />
-        </div>
-      </Drawer>
-      <main className={classes.content}>
-        <ThemeProvider theme={theme}>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={ListProjects} />
-            </Switch>
-          </Router>
-          <CssBaseline />
-        </ThemeProvider>
-      </main>
-    </div>
+    <div className={classes.root}>Test deployment storybook in github page</div>
   );
 }
-
 export default App;
--- src/App/style.js
+++ src/App/style.js
@@ -1,85 +1,11 @@
 import { makeStyles } from "@material-ui/core";
-const drawerWidth = 160;
-const styles = makeStyles((theme) => ({
-  appMain: {
-    width: "100%",
-  },
+const styles = makeStyles({
   root: {
     display: "flex",
     "& .MuiListItem-root": {
       color: "white",
     },
   },
-
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-
-  hide: {
-    display: "none",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap",
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    backgroundColor: "#18202c",
-
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: "hidden",
-    backgroundColor: "#18202c",
-
-    width: theme.spacing(4) + 1,
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(6) + 1,
-    },
-  },
-  toolbarOpen: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-  },
-  toolbarClose: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    overflowX: "hidden",
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  listItem: {
-    minWidth: "35px",
-    paddingRight: "12px",
-    color: "white",
-  },
-  linkTex: {
-    textDecoration: "none",
-    color: "white",
-  },
-}));
+});
 
 export default styles;
